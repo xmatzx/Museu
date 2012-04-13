2967893e7c810ef53ec7175f7c21cf1cf45f1a8a
Miguel <Miguel@Miguel-TOSH>
2012-04-13 12:08:42 +0100

    Visita Guiada e contactos.

---
+++ apps/frontend/modules/catalogo/templates/indexSuccess.php
@@ -0,0 +1,147 @@
+<div id="main">
+    <div class="row-fluid">
+  <div data-target="#demo" data-toggle="collapse" id="catalog-menu" class="well">
+    <i class="icon-resize-full"></i>
+    
+    
+    <div class="collapse out" id="demo">
+      <div style="background: red;" class="hero-unit">
+        <h1>Simple and effective</h1>
+      </div>
+    </div>
+  </div>
+
+  <div class="collapse out" id="demo">
+    <div style="background: red;" class="hero-unit">
+      <h1>Simple and effective</h1>
+    </div>
+  </div>
+
+</div>
+
+<div class="row-fluid">
+  <div class="well">
+    <h1>catalogo</h1>
+  </div>
+</div>
+
+<div class="row-fluid">
+  <div class="pagination pagination-right">
+    <ul>
+      <li><a href="#">←</a></li>
+      <li class="active">
+        <a href="#">1</a>
+      </li>
+      <li><a href="#">2</a></li>
+      <li><a href="#">3</a></li>
+      <li><a href="#">4</a></li>
+      <li><a href="#">→</a></li>
+    </ul>
+  </div>
+</div>
+
+<div class="row-fluid">
+  <ul class="thumbnails">
+    <li class="span4">
+      <a class="thumbnail" href="#">
+        <img alt="" src="http://placekitten.com/g/360/268">
+      </a>
+    </li>
+    <li class="span4">
+      <a class="thumbnail" href="#">
+        <img alt="" src="http://placehold.it/360x268">
+      </a>
+    </li>
+    <li class="span4">
+      <a class="thumbnail" href="#">
+        <img alt="" src="http://placekitten.com/g/360/268">
+      </a>
+    </li>
+    <li class="span3">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+    <li class="span3">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+    <li class="span3">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+    <li class="span3">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+    <li class="span2">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+    <li class="span2">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+    <li class="span2">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+    <li class="span2">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+    <li class="span2">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+    <li class="span2">
+      <div class="thumbnail">
+        <img alt="" src="http://placehold.it/260x180">
+        <h5>Thumbnail label</h5>
+        <p>Thumbnail caption right here...</p>
+      </div>
+    </li>
+  </ul>
+</div>
+
+<div class="row-fluid">
+    <div class="pagination pagination-right">
+      <ul>
+        <li><a href="#">←</a></li>
+        <li class="active">
+          <a href="#">1</a>
+        </li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">→</a></li>
+      </ul>
+    </div>
+</div>
+    </div>
---
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -0,0 +1,63 @@
+<div id="main">
+    <div class="row">
+  <div class="carousel" id="myCarousel">
+    <!-- Carousel items -->
+    <div class="carousel-inner">
+      <div class="active item">
+        <img style="width: 100%; max-height: 200px;" src="img/objecto3.png">
+      </div>
+      <div class="item">
+        <img style="width: 100%; max-height: 200px;" src="img/objecto4.png">
+      </div>
+      <div class="item">
+        <img style="width: 100%; max-height: 200px;" src="img/objecto5.png">
+      </div>
+    </div>
+    <!-- Carousel nav -->
+    <a data-slide="prev" href="#myCarousel" class="carousel-control left">‹</a>
+    <a data-slide="next" href="#myCarousel" class="carousel-control right">›</a>
+  </div>
+</div><!-- row -->
+
+<div class="row">
+  <div class="span2">
+    <div class="well">
+      <img style="width: 100%" src="http://placekitten.com/g/200/200">
+    </div>
+  </div>
+  <div class="span10">
+    <div class="well">
+      O melhor museu do ISEP inteiro
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="span4">
+    <div class="well">
+      <h5>Últimas Notícias</h5>
+      <hr>
+      <a href="/contactos">Contactos</a>
+    </div>
+  </div>
+  <div class="span4">
+    <div class="well">
+      <h5>Últimas Peças</h5>
+      <hr>
+      <a href="/contactos">Contactos</a>
+    </div>
+  </div>
+  <div class="span4">
+    <div style="background: orange;" class="well">
+      <h5><i class="icon-info-sign icon-white"></i> Informações Úteis</h5>
+      <hr>
+      <h6>Morada</h6>
+      <p>Morada do sítio em questão</p>
+      
+      <h6>Telefone</h6>
+      <p>Telefone do sítio em questão</p>
+    </div>
+  </div>
+</div>
+
+    </div>
