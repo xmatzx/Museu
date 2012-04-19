79a822ee0aec32af4f90691f799617fc5d513866
matz <matz@ubuntu.ubuntu-domain>
2012-04-19 23:29:16 +0100

    - posicionamento conteudo

--- apps/frontend/modules/contacto/templates/indexSuccess.php
+++ apps/frontend/modules/contacto/templates/indexSuccess.php
@@ -1,19 +1,40 @@
-<div id="InfoGeral" class="well">
-    <h2><?php echo __('Horário de Funcionamento') ?></h2>
+<div style="width: 100%;">
+  <div class="row">
+    <div style="width: 45% !important;" class="span4">
+      <div class="well">
+        <h5><?php echo __('Horário de Funcionamento') ?></h5>
+        <hr/>
+        <div><?php echo __('Segunda a Sexta-feira') ?></div>
 
-    <div><?php echo __('Segunda a Sexta-feira') ?></div>
+        <div>09:30h » 12:30h</div>
 
-    <div>09:30h » 12:30h</div>
+        <div>14:00h » 17:00h</div>
 
-    <div>14:00h » 17:00h</div>
-
-    <div><?php echo __('Entrada Gratuita') ?></div>
-    <br/>
-    <h2><?php echo __('Marcação de Grupos')?></h2>
-
-    <div>tel.:<a href="">+351 228 340 508</a></div>
-
-    <div>fax: <a href="">+351 228 321 159</a></div>
-
-    <div>e-mail: <a href="mailto:museu@isep.ipp.pt">museu@isep.ipp.pt</a></div>
-</div>
+        <div><b><?php echo __('Entrada Gratuita') ?><b/></div>
+      </div>
+    </div>
+    <div style="width: 50% !important;" class="span4">
+      <div class="well">
+        <h5><?php echo __('Marcação de Grupos') ?></h5>
+        <hr/>
+        <div class="row-fluid">
+          <table style="text-align: left;width: 100%;">
+            <tbody>
+              <tr>
+                <th><?php echo __('Telefone:') ?></th>
+                <td>+351 228 340 508</td>
+              </tr>
+              <tr>
+                <th><?php echo __('Fax:') ?></th>
+                <td>+351 228 321 159</td>
+              </tr>
+              <tr>
+                <th><?php echo __('E-mail:') ?></th>
+                <td><a href="mailto:museu@isep.ipp.pt">museu@isep.ipp.pt</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
