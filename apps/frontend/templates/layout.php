d5d7b34fb3df076d717362e0315e4df974471d5c
matz <matz@ubuntu.ubuntu-domain>
2012-04-21 14:55:23 +0100

    - mensagens alerta

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -11,6 +11,19 @@
     <?php use_javascript('popcorn.js')?>
     <?php include_javascripts() ?>
     <script type="text/javascript">
+      $(document).ready(function() {
+        $(".ui-state-highlight").animate({
+            opacity: 1,
+            height: 'toggle'
+          }, 2000, function() {
+        });
+        $(".ui-state-error").animate({
+            opacity: 1,
+            height: 'toggle'
+          }, 2000, function() {
+        });
+      });
+      
       function setCulture(language)
       {
         $.ajax({
@@ -64,6 +77,36 @@
       </div><!-- navbar -->
 
       <div id="main">
+        <div style="padding: 15px;">
+          <?php if ($sf_user->hasFlash('success')): ?>
+              <div id="noticeFlash" class="ui-pnotify ui-widget ui-helper-clearfix ui-helper-style">
+                <div class="ui-pnotify-shadow ui-widget-shadow"></div>
+                <div class="ui-pnotify-container ui-corner-all ui-state-highlight" style="min-height: 16px;">
+                  <div class="ui-pnotify-closer" style="cursor: pointer; display: none;">
+                    <span class="ui-icon ui-icon-circle-close"></span>
+                  </div>
+                  <div class="ui-pnotify-icon">
+                    <span class="ui-icon ui-icon-info"><?php echo image_tag('icons/notify_info.png') ?></span>
+                  </div>
+                  <div class="ui-pnotify-text"><?php echo $sf_user->getFlash('success') ?></div>
+                </div>
+              </div>
+            <?php endif; ?>
+            <?php if ($sf_user->hasFlash('error')): ?>
+              <div id="errorFlash" class="ui-pnotify ui-widget ui-helper-clearfix ui-helper-style">
+                <div class="ui-pnotify-shadow ui-widget-shadow"></div>
+                <div class="ui-pnotify-container ui-corner-all ui-state-error" style="min-height: 16px;">
+                  <div class="ui-pnotify-closer" style="cursor: pointer; display: none;">
+                    <span class="ui-icon ui-icon-circle-close"></span>
+                  </div>
+                  <div class="ui-pnotify-icon">
+                    <span class="ui-icon ui-icon-info"><?php echo image_tag('icons/notify_error.png') ?></span>
+                  </div>
+                  <div class="ui-pnotify-text"><?php echo $sf_user->getFlash('error') ?></div>
+                </div>
+              </div>
+            <?php endif; ?>
+        </div>
         <?php echo $sf_content ?>
       </div>
 
